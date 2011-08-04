95fb5bee7a83eb9f749401b72a29fe8684630704
stephenmcd <steve@jupo.org>
2011-08-04 21:13:51 +1000

    Fixed signal tests.

--- django_socketio/tests.py
+++ django_socketio/tests.py
@@ -21,7 +21,7 @@ class MockSocketIo(object):
     def recv(self):
         """
         Return data once so that the ``on_message`` signal
-        can be triggered.
+        can be triggered when testing.
         """
         if not self.recv_once:
             self.recv_once = True
@@ -50,31 +50,42 @@ class Tests(TestCase):
     django-socketio tests.
     """
 
-    def test_view(self):
+    def test_signals_and_response(self):
         """
-        Ensure the socketio view doesn't return errors.
+        Ensure that the socketio view returns a valid response and
+        that signals are received. Signals are checked by pulling
+        each event name off a list of events. The error event should
+        be the only remaning. The connect signal is used to remove
+        any non-test message handlers since there's no guarantee the
+        mock socketio object will return the data they expect.
         """
+
+        events = ["connect", "message", "disconnect", "finish", "error"]
+        test_uid = "message_test"
+
+        @receiver(signals.on_connect)
+        def on_connect(sender, **kwargs):
+            r = [r for r in signals.on_message.receivers if r[0][0] == test_uid]
+            signals.on_message.receivers = r
+            events.remove("connect")
+
+        @receiver(signals.on_message, dispatch_uid=test_uid)
+        def on_message(sender, **kwargs):
+            events.remove("message")
+
+        @receiver(signals.on_disconnect)
+        def on_disconnect(sender, **kwargs):
+            events.remove("disconnect")
+
+        @receiver(signals.on_finish)
+        def on_finish(sender, **kwargs):
+            events.remove("finish")
+
+        @receiver(signals.on_error)
+        def on_error(sender, **kwargs):
+            events.remove("error")
+
         response = SocketIoClient().get(reverse("socketio"))
         self.assertEqual(response.status_code, 200)
-
-    def test_signals(self):
-        """
-        Ensure the socketio signals are received.
-        """
-        events = ["connect", "message", "disconnect", "finish"]
-
-        on_connect = lambda **kwargs: events.remove("connect")
-        receiver(signals.on_connect)(on_connect)
-
-        on_message = lambda **kwargs: events.remove("message")
-        receiver(signals.on_message)(on_message)
-
-        on_disconnect = lambda **kwargs: events.remove("disconnect")
-        receiver(signals.on_disconnect)(on_disconnect)
-
-        on_finish = lambda **kwargs: events.remove("finish")
-        receiver(signals.on_finish)(on_finish)
-
-        response = SocketIoClient().get(reverse("socketio"))
-        self.assertEqual(len(events), 0)
-
+        self.assertEqual(len(events), 1)
+        self.assertEqual(events[0], "error")
