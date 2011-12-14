ceb7fb3e09e024a9a0dce93c49da5194c4c9b332
Stephen McDonald <steve@jupo.org>
2011-12-14 22:26:41 +1100

    Changed to dict lookup in oob broadcast.

--- django_socketio/__init__.py
+++ django_socketio/__init__.py
@@ -29,13 +29,7 @@ def broadcast_channel(message, channel):
     from django_socketio.views import CLIENTS
     from django_socketio.channels import CHANNELS
     try:
-        first_session_for_channel = CHANNELS.get(channel, [])[0]
-    except IndexError:
-        raise NoSockets()
-    else:
-        for (request, socket, context) in CLIENTS.values():
-            if socket.session.session_id == first_session_for_channel:
-                break
-        else:
-            raise NoSockets("There are no clients on the channel: " + channel)
+        socket = CLIENTS[CHANNELS.get(channel, [])[0]][1]
+    except IndexError, KeyError:
+        raise NoSockets("There are no clients on the channel: " + channel)
     socket.send_and_broadcast_channel(message, channel)
