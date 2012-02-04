a4add05e3e917ac0f4a3045ae24477dd47d82a7f
Stephen McDonald <steve@jupo.org>
2012-02-05 05:49:40 +1100

    Fall back to the port in settings if none provided in the environment. Closes #15.

--- django_socketio/templatetags/socketio_tags.py
+++ django_socketio/templatetags/socketio_tags.py
@@ -3,10 +3,12 @@ from os import environ
 
 from django.template import Library
 
+from django_socketio.settings import PORT
+
 
 register = Library()
 
 @register.inclusion_tag("socketio_scripts.html", takes_context=True)
 def socketio(context):
-    context["DJANGO_SOCKETIO_PORT"] = environ["DJANGO_SOCKETIO_PORT"]
+    context["DJANGO_SOCKETIO_PORT"] = environ.get("DJANGO_SOCKETIO_PORT", PORT)
     return context
