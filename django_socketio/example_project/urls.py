b0d2a519338ae1e34b868f2a00abf9ecaf30a87d
Stephen McDonald <steve@jupo.org>
2013-04-06 04:02:24 +1100

    Django 1.5 support

--- django_socketio/example_project/urls.py
+++ django_socketio/example_project/urls.py
@@ -1,8 +1,10 @@
 
 from django.conf import settings
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
--- django_socketio/urls.py
+++ django_socketio/urls.py
@@ -1,5 +1,5 @@
 
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from django.conf import settings
 from django.utils.importlib import import_module
 
